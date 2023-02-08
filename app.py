ad3b7cb3590c1c72431ecd2f370c23e6f0d07a7f
Abhishek Patnaik <abhishekpatnaik77@gmail.com>
2023-02-08 12:34:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from seoanalyzer import analyze
 import json
 from flask import Flask, request, jsonify 
-
+from flask_cors import CORS, cross_origin
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 app = Flask(__name__)
 
 @app.route('/get-url-info/', methods=['GET'])
+@cross_origin()
 def respond():
     query = request.args.get("q")
     output = analyze(query)
